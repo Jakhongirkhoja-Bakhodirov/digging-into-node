8376c3f9e6c87f712114a848346a87a739b0f3aa
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-03-30 16:52:00 +0500

    argument handling

--- cmd-argv.js
+++ cmd-argv.js
@@ -8,6 +8,28 @@ const argv = require('minimist')(process.argv.slice(2) , {
 
 // process.stdin.read();
 
+if(argv.help) {
+    printHelp();
+} else if(argv.file) {
+    console.log(argv.file);
+} else {
+    error('Incorrect usage cmd arguments' , true);
+}
+
+function error(message,includeHelp = false) {
+    console.error(message);
+    if(includeHelp) {
+        console.log("");
+        printHelp();
+    }
+}
+
+function printHelp() {
+    console.log("ex1 usage:");
+    console.log("ex1.js --help");
+    console.log("--help");
+}
+
 console.log('Something New');
 console.error('Oops');
 
